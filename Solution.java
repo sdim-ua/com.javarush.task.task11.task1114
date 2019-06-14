5941a414e219b593ac606e4f702463b72f61b874
sdim-ua <ingersol@yandex.ua>
2019-06-14 10:30:36 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+/* 
+Все мировые религии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Judaism {
+
+    }
+
+    public class Christianity extends Judaism  {
+
+    }
+
+    public class Islam extends Christianity {
+
+    }
